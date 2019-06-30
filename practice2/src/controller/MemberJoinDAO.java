27de47c05979598cc57bed8a83c6b866aea46e4a
wj2kim <wj2kim@gmail.com>
2019-07-01 01:38:15 +0900

    update version

--- practice2/src/controller/MemberJoinDAO.java
+++ practice2/src/controller/MemberJoinDAO.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 import member.vo.Member;
@@ -9,6 +10,8 @@ public class MemberJoinDAO {
 	
 	Scanner sc= new Scanner (System.in);
 	ArrayList<Member> list=new ArrayList<Member>();
+	static MemberJoinDAO mjd= new MemberJoinDAO();
+	
 	
 	public MemberJoinDAO() {
 
@@ -19,10 +22,16 @@ public class MemberJoinDAO {
 		while ((key=menu())!=0){
 			switch(key) {
 			case 1:
+				login();
 				break;
 			case 2:
+//				register();
 				break;
 			case 3:
+				displayMember();
+				break;
+			case 4:
+//				searchMember();
 				break;
 			}
 		}
@@ -33,9 +42,35 @@ public class MemberJoinDAO {
 		return getIntInput ("[1]Log In [2]Join [3]Search [0]Exit");
 	}
 	
+	public void displayMember() {
+		Iterator<Member> ite=list.iterator();
+		if(ite.hasNext()) {
+			System.out.println(ite.next());
+		}
+	}
+	
+	public void login() {
+		sc.nextLine();
+		String id=getStrInput("ID: ");
+		String pw=getStrInput("pw: ");
+		
+		for (Member m : list) {
+			if(m.getId().equals(id)&&m.getPw().equals(pw)){
+				System.out.println("Welcome User "+ m.getId());
+			}
+			
+		}
+		
+	}
+	
 	public int getIntInput(String msg) {
 		System.out.println(msg);
 		return sc.nextInt();
+	}
+	
+	public String getStrInput(String msg) {
+		System.out.println(msg);
+		return sc.nextLine();
 	}
 	
 	
--- practice2/src/main/Main.java
+++ practice2/src/main/Main.java
@@ -8,6 +8,7 @@ public class Main {
 		
 		MemberJoinDAO mj =new MemberJoinDAO();
 		mj.run();
+
 		
 	}
 
