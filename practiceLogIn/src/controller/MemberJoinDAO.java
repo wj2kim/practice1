fbe7f77a61bb24049a5501a29042d89c7c26eb98
wj2kim <wj2kim@gmail.com>
2019-07-22 12:40:46 +0900

    practice Login update

---
+++ practiceLogIn/src/controller/MemberJoinDAO.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.lang.reflect.Member;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MemberJoinDAO {
+	
+	Scanner sc = new Scanner (System.in);
+	ArrayList<Member> list =new ArrayList<Member>();
+	
+	public MemberJoinDAO() {
+	
+	}
+
+}
